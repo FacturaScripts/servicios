3bc9c763350e670900d88f0594b7dd10306e4374
Yopli2k <yopli2000@gmail.com>
2021-07-01 00:21:41 +0200

    Add the printing of new sections.

--- Lib/Export/PDFserviciosExport.php
+++ Lib/Export/PDFserviciosExport.php
@@ -25,12 +25,13 @@ use FacturaScripts\Plugins\Servicios\Model\ServicioAT;
  * Description of PDFserviciosExport
  *
  * @author Carlos Garcia Gomez <carlos@facturascripts.com>
+ * @author Jose Antonio Cuello Principal <yopli2000@gmail.com>
  */
 class PDFserviciosExport extends \FacturaScripts\Core\Lib\Export\PDFExport
 {
 
     /**
-     * 
+     *
      * @param ServicioAT $model
      * @param array      $columns
      * @param string     $title
@@ -39,16 +40,117 @@ class PDFserviciosExport extends \FacturaScripts\Core\Lib\Export\PDFExport
      */
     public function addModelPage($model, $columns, $title = ''): bool
     {
-        $this->newPage();
+        $subject = $model->getSubject();
         $idempresa = isset($model->idempresa) ? $model->idempresa : null;
-        $this->insertHeader($idempresa);
 
+        $this->newPage();
+        $this->insertHeader($idempresa);
         $this->pdf->ezText("\n" . $title . ': ' . $model->idservicio . "\n", self::FONT_SIZE + 6);
         $this->newLine();
 
-        $subject = $model->getSubject();
+        $this->insertParalellTable($this->serviceData($model, $subject), '', $this->tableOptions());
+        $this->pdf->ezText('');
+
+        if ($this->toolBox()->appSettings()->get('servicios', 'machines', false)) {
+            $this->printTableSection('machines', $this->machinesData($model));
+        }
+
+        $this->printTextSection('description', $model->descripcion);
+        $this->printTextSection('material', $model->material);
+        $this->printTextSection('solution', $model->solucion);
+
+        if ($this->toolBox()->appSettings()->get('servicios', 'notes', false)) {
+            $this->printTextSection('notes', $model->observaciones);
+        }
+
+        if ($this->toolBox()->appSettings()->get('servicios', 'works', false)) {
+            $this->printTableSection('works', $this->worksData($model));
+        }
+
+        $footer = $this->toolBox()->appSettings()->get('servicios', 'footer', '');
+        $this->printTextSection("", $footer, false);
+
+        return false;
+    }
+
+    /**
+     * Print a section with an array of data.
+     *
+     * @param string $title
+     * @param array $data
+     */
+    protected function printTableSection($title, $data)
+    {
+        $this->pdf->ezText("\n" . $this->i18n->trans($title) . "\n", self::FONT_SIZE + 4);
+        $this->newLine();
+        $this->pdf->ezTable($data, '', '', $this->tableOptions(1));
+        $this->pdf->ezText('');
+
+    }
+
+    /**
+     * Print a section with a text data.
+     *
+     * @param string $title
+     * @param string $text
+     */
+    protected function printTextSection($title, $text, $addLine = true)
+    {
+        if (empty($text)) {
+            return;
+        }
+
+        $this->pdf->ezText("\n" . $this->i18n->trans($title) . "\n", self::FONT_SIZE + 4);
+        if ($addLine) {
+            $this->newLine();
+        }
+        $this->pdf->ezText(\nl2br($text) . "\n", self::FONT_SIZE + 2);
+    }
+
+    /**
+     *
+     * @return array
+     */
+    protected function tableOptions($headings = 0):array
+    {
+        return [
+            'width' => $this->tableWidth,
+            'showHeadings' => $headings,
+            'shaded' => 0,
+            'lineCol' => [1, 1, 1],
+            'cols' => []
+        ];
+    }
+
+    /**
+     *
+     * @param ServicioAT $model
+     * @return array
+     */
+    private function machinesData(&$model):array
+    {
+        $result = [];
+        foreach ($model->getMachines() as $machine) {
+            $result[] = [
+                $this->i18n->trans('name') => $machine->nombre,
+                $this->i18n->trans('serial-number') => $machine->numserie,
+                $this->i18n->trans('description') => $machine->descripcion,
+            ];
+        }
+        return $result;
+    }
+
+    /**
+     *
+     * @param ServicioAT $model
+     * @param Cliente $subject
+     * @return array
+     */
+    private function serviceData(&$model, &$subject):array
+    {
         $tipoidfiscal = empty($subject->tipoidfiscal) ? $this->i18n->trans('cifnif') : $subject->tipoidfiscal;
-        $tableData = [
+
+        return [
             ['key' => $this->i18n->trans('date'), 'value' => $model->fecha],
             ['key' => $this->i18n->trans('hour'), 'value' => $model->hora],
             ['key' => $this->i18n->trans('customer'), 'value' => Utils::fixHtml($subject->nombre)],
@@ -56,35 +158,25 @@ class PDFserviciosExport extends \FacturaScripts\Core\Lib\Export\PDFExport
             ['key' => $this->i18n->trans('address'), 'value' => $subject->getDefaultAddress()->direccion],
             ['key' => $this->i18n->trans('phone'), 'value' => $subject->telefono1]
         ];
+    }
 
-        $tableOptions = [
-            'width' => $this->tableWidth,
-            'showHeadings' => 0,
-            'shaded' => 0,
-            'lineCol' => [1, 1, 1],
-            'cols' => []
-        ];
-        $this->insertParalellTable($tableData, '', $tableOptions);
-        $this->pdf->ezText('');
-
-        if ($model->descripcion) {
-            $this->pdf->ezText("\n" . $this->i18n->trans('description') . "\n", self::FONT_SIZE + 4);
-            $this->newLine();
-            $this->pdf->ezText(\nl2br($model->descripcion) . "\n", self::FONT_SIZE + 2);
+    /**
+     *
+     * @param ServicioAT $model
+     * @return array
+     */
+    private function worksData(&$model):array
+    {
+        $result = [];
+        foreach ($model->getTrabajos() as $work) {
+            $result[] = [
+                $this->i18n->trans('from-date') => $work->fechainicio,
+                $this->i18n->trans('from-hour') => $work->horainicio,
+                $this->i18n->trans('until-date') => $work->fechafin,
+                $this->i18n->trans('until-hour') => $work->horafin,
+                $this->i18n->trans('observations') => $work->observaciones,
+            ];
         }
-
-        if ($model->material) {
-            $this->pdf->ezText("\n" . $this->i18n->trans('material') . "\n", self::FONT_SIZE + 4);
-            $this->newLine();
-            $this->pdf->ezText(\nl2br($model->material) . "\n", self::FONT_SIZE + 2);
-        }
-
-        if ($model->solucion) {
-            $this->pdf->ezText("\n" . $this->i18n->trans('solution') . "\n", self::FONT_SIZE + 4);
-            $this->newLine();
-            $this->pdf->ezText(\nl2br($model->solucion) . "\n", self::FONT_SIZE + 2);
-        }
-
-        return false;
+        return $result;
     }
 }
