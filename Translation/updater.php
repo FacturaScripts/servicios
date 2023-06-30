04b13b5dad95d6213c6c7d6ed4fbb90161dbee67
Carlos Garcia Gomez <neorazorx@gmail.com>
2023-06-30 18:58:48 +0200

    Actualizadas las traducciones.

--- Translation/updater.php
+++ Translation/updater.php
@@ -3,10 +3,10 @@ if (php_sapi_name() !== "cli") {
     die("Please use command line: php updater.php");
 }
 
-/// scan json files
+// scan json files
 chdir(__DIR__);
 $files = [];
-$langs = 'ca_ES,de_DE,en_EN,es_AR,es_CL,es_CO,es_CR,es_DO,es_EC,es_ES,es_GT,es_MX,es_PE,es_UY,eu_ES,fr_FR,gl_ES,it_IT,pt_PT,va_ES';
+$langs = 'ca_ES,cs_CZ,de_DE,en_EN,es_AR,es_CL,es_CO,es_CR,es_DO,es_EC,es_ES,es_GT,es_MX,es_PA,es_PE,es_UY,eu_ES,fr_FR,gl_ES,it_IT,pl_PL,pt_BR,pt_PT,va_ES';
 foreach (explode(',', $langs) as $lang) {
     $files[] = $lang . '.json';
 }
@@ -16,16 +16,27 @@ foreach (scandir(__DIR__, SCANDIR_SORT_ASCENDING) as $filename) {
     }
 }
 
-/// download json from facturascripts.com
+// download json from facturascripts.com
 foreach ($files as $filename) {
-    $url = "https://facturascripts.com/EditLanguage?action=json&idproject=93&code=";
-    $json = file_get_contents($url . substr($filename, 0, -5));
-    if (empty($json) || strlen($json) < 10) {
-        unlink($filename);
-        echo "Remove " . $filename . "\n";
+    $url = "https://facturascripts.com/EditLanguage?action=json&idproject=93&code=" . substr($filename, 0, -5);
+    $newContent = file_get_contents($url);
+    if (empty($newContent)) {
+        if (file_exists($filename)) {
+            unlink($filename);
+            echo "Remove " . $filename . "\n";
+            continue;
+        }
+
+        echo "Empty " . $filename . "\n";
         continue;
     }
 
-    echo "Download " . $filename . "\n";
-    file_put_contents($filename, $json);
+    $oldContent = file_exists($filename) ? file_get_contents($filename) : '';
+    if (strlen($newContent) > 10 && $newContent !== $oldContent) {
+        echo "Download " . $filename . "\n";
+        file_put_contents($filename, $newContent);
+        continue;
+    }
+
+    echo "Skip " . $filename . "\n";
 }
