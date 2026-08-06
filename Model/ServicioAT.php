244cd8a18a2b9d86c068d29709fa3029edec50b3
Esteban Sánchez <estebansm85@gmail.com>
2026-08-06 13:47:26 +0200

    Actualiza la lógica de actualización de teléfonos en ServicioAT
    - Modifica la función de cambio de cliente para que solo actualice
      el teléfono 2 si no ha sido modificado manualmente.
    - Añade una nueva prueba en ServicioAtTest para verificar el
      comportamiento esperado al cambiar de cliente con teléfonos
      distintos.
    
    Archivos modificados:
    - ServicioAT.php
    - ServicioAtTest.php

--- Model/ServicioAT.php
+++ Model/ServicioAT.php
@@ -367,9 +367,11 @@ class ServicioAT extends ModelClass
             // si cambiamos el cliente y los teléfonos no se han modificado manualmente,
             // los actualizamos con los del nuevo cliente
             $oldCustomer = $this->getCustomer($this->getOriginal('codcliente') ?? '');
-            if ($this->telefono1 == $oldCustomer->telefono1 && $this->telefono2 == $oldCustomer->telefono2) {
-                $customer = $this->getSubject();
+            $customer = $this->getSubject();
+            if ($this->telefono1 == $oldCustomer->telefono1) {
                 $this->telefono1 = $customer->telefono1;
+            }
+            if ($this->telefono2 == $oldCustomer->telefono2) {
                 $this->telefono2 = $customer->telefono2;
             }
         }
